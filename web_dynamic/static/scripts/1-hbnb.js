ed8c37d2a1585cb52e91d2366395423171207cff
IOLevi <337@holbertonschool.com>
2018-09-08 14:23:45 -0700

    2 is working

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,17 @@
+let mtotal = [];
 $(document).ready(() => {
-  
-
+  $('.amenities input').change(function () {
+    if ($(this).prop('checked')) {
+      mtotal.push($(this).attr('data-name'));
+    }
+    else {
+      let v = $(this).attr('data-name');
+      let ind = mtotal.indexOf(v);
+      if (ind > -1) {
+        mtotal.splice(ind, 1);
+      }
+    }
+    mtotal.sort();
+    $('.amenities h4').text(mtotal.join(', '));
+  });
 });
