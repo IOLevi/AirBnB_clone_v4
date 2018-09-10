451ca8ca68d219ea97a652078d5af07e7b4dafce
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-09-10 07:50:08 +0000

    working on 4 and 5

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,40 @@
+let mtotal = [];
+$(document).ready(() => {
+  $('.amenities input').change(function () {
+    if ($(this).prop('checked')) {
+      mtotal.push($(this).attr('data-name'));
+    } else {
+      let v = $(this).attr('data-name');
+      let ind = mtotal.indexOf(v);
+      if (ind > -1) {
+        mtotal.splice(ind, 1);
+      }
+    }
+    mtotal.sort();
+    $('.amenities h4').text(mtotal.join(', '));
+  });
+  $.get('http://0.0.0.0:5001/api/v1/status',
+    function (data, textStatus) {
+      if (textStatus === '200') {
+        $('DIV#api_status').addClass('available');
+      } else {
+        $('DIV#api_status').removeClass('available');
+      }
+    }
+  );
+});
+  $.ajax({
+      url:'http://0.0.0.0:5001/api/v1/places_search/',
+      type:'POST',
+      data:data,
+      contentType:'application/json',
+      dataType:'json',
+      success: function(data) {
+	   $.ajax({
+	       url:'http://0.0.0.0:5001/api/v1/places_search',
+	       type:'POST',
+	       data:data,
+	       contentType:'application/json',
+	       dataType:'json',
+	   });
+	});
