318b6f4f0b6271ce057c40d1ffff0c334b709052
IOLevi <337@holbertonschool.com>
2018-09-10 14:38:59 -0700

    4 is working

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -13,28 +13,36 @@ $(document).ready(() => {
     mtotal.sort();
     $('.amenities h4').text(mtotal.join(', '));
   });
-  $.get('http://0.0.0.0:5001/api/v1/status',
+  $.get('http://localhost:5001/api/v1/status',
     function (data, textStatus) {
-      if (textStatus === '200') {
+      if (data['status'] === 'OK') {
         $('DIV#api_status').addClass('available');
       } else {
         $('DIV#api_status').removeClass('available');
       }
     }
   );
+  $.ajax({url: 'http://localhost:5001/api/v1/places_search/',
+    type: 'POST',
+    data: '{}',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: (data) => {
+      for (let place of data) {
+        $('SECTION.places').append('<article> <div class="title"> <h2>' +
+        '#' + place.name + '</h2>' + '<div class="price_by_night">' + '$' + 
+        place.price_by_night + '</div> </div>' +
+        '<div class="information"> <div class="max_guest">' +
+        '<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+        '<br />' + place.max_guest + ' Guests' +
+        '</div>' +
+        '<div class="number_rooms"> <i class="fa fa-bed fa-3x" aria-hidden="true">' +
+        '</i><br />' + place.number_rooms + ' Bedrooms' + '</div>' +
+        '<div class="number_bathrooms"> <i class="fa fa-bed fa-3x" aria-hidden="true">' +
+        '</i><br />' + place.number_bathrooms + ' Bathrooms' + '</div> </div>' +
+        '<div class="description">' + place.description + '</div>' +
+        '</article>');
+      }
+    }
+  });
 });
-  $.ajax({
-      url:'http://0.0.0.0:5001/api/v1/places_search/',
-      type:'POST',
-      data:data,
-      contentType:'application/json',
-      dataType:'json',
-      success: function(data) {
-	   $.ajax({
-	       url:'http://0.0.0.0:5001/api/v1/places_search',
-	       type:'POST',
-	       data:data,
-	       contentType:'application/json',
-	       dataType:'json',
-	   });
-	});
